fa37018c429a78f88abcce35af762708fb685cf4
littlezethstar <littlezethstar@gmail.com>
2021-12-01 08:33:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function calculate() {
+    let height = document.getElementById("height_input").value;
+    let weight = document.getElementById("weight_input").value;
+    let bmi = 0;
+    bmi = (weight / (height * height));
+    bmi = bmi.toFixed(2);
+    document.getElementsByName('display')[0].value = bmi;
+}
